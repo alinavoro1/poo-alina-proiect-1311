ce6112bc11764897c5eaa3395b57c2b443a87082
alinavoro1 <avoronetchi8@gmail.com>
2025-03-01 14:38:13 +0200

    Initialize http-server

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <array>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+
+// #define CPPHTTPLIB_OPENSSL_SUPPORT
+#include <httplib.h>
+
+// #include <Helper.h>
+
+int main() {
+    // HTTP
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &req, httplib::Response &res) {
+      std::cout << "[server] Got req on path " << req.path << "\n";
+      res.set_content("Hello World!", "text/plain");
+    });
+
+    if (std::getenv("CI")) {
+        std::thread stopper([&](){
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            std::cout << "[server] Shutting down server\n";
+            svr.stop();
+        });
+        stopper.detach();
+    }
+    std::cout << "[server] Starting server\n";
+    svr.listen("0.0.0.0", 8080);
+    // std::cout << "Hello, world!\n";
+    // std::array<int, 100> v{};
+    // int nr;
+    // std::cout << "Introduceți nr: ";
+    /////////////////////////////////////////////////////////////////////////
+    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
+    /// dați exemple de date de intrare folosind fișierul tastatura.txt
+    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
+    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
+    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
+    /// pentru cât mai multe ramuri de execuție.
+    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
+    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
+    ///
+    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
+    /// pentru a simula date introduse de la tastatură.
+    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
+    ///
+    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
+    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
+    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
+    /// program care merg (și să le evitați pe cele care nu merg).
+    ///
+    /////////////////////////////////////////////////////////////////////////
+    // std::cin >> nr;
+    /////////////////////////////////////////////////////////////////////////
+    // for(int i = 0; i < nr; ++i) {
+    //     std::cout << "v[" << i << "] = ";
+    //     std::cin >> v[i];
+    // }
+    // std::cout << "\n\n";
+    // std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
+    // for(int i = 0; i < nr; ++i) {
+    //     std::cout << "- " << v[i] << "\n";
+    // }
+    ///////////////////////////////////////////////////////////////////////////
+    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
+    /// alt fișier propriu cu ce alt nume doriți.
+    /// Exemplu:
+    /// std::ifstream fis("date.txt");
+    /// for(int i = 0; i < nr2; ++i)
+    ///     fis >> v2[i];
+    ///
+    ///////////////////////////////////////////////////////////////////////////
+    ///                Exemplu de utilizare cod generat                     ///
+    ///////////////////////////////////////////////////////////////////////////
+    // Helper helper;
+    // helper.help();
+    ///////////////////////////////////////////////////////////////////////////
+    return 0;
+}
