02137f97161e4c8f1443474b65881c52db6a1c0e
alinavoro1 <avoronetchi8@gmail.com>
2025-03-01 14:38:15 +0200

    Initialize sfml3.0-resurse-locale

---
+++ main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <thread>
+
+#include <SFML/Graphics.hpp>
+
+#include <Helper.h>
+
+//////////////////////////////////////////////////////////////////////
+/// NOTE: this include is needed for environment-specific fixes     //
+/// You can remove this include and the call from main              //
+/// if you have tested on all environments, and it works without it //
+#include "env_fixes.h"                                              //
+//////////////////////////////////////////////////////////////////////
+
+#include "ResourceManager.hpp"
+
+
+//////////////////////////////////////////////////////////////////////
+/// This class is used to test that the memory leak checks work as expected even when using a GUI
+class SomeClass {
+public:
+    explicit SomeClass(int) {}
+};
+
+SomeClass *getC() {
+    return new SomeClass{2};
+}
+//////////////////////////////////////////////////////////////////////
+
+void PersonalizeazaAvion(sf::Sprite& avion)
+{
+    avion.setScale(sf::Vector2f(0.3f, 0.3f));
+    avion.setPosition(sf::Vector2f(400, 350));
+
+    sf::FloatRect bounds = avion.getGlobalBounds();
+    avion.setOrigin(sf::Vector2f(
+        bounds.position.x + bounds.size.x / 2,
+        bounds.position.y + bounds.size.y / 2)
+    );
+}
+
+int main() {
+    ////////////////////////////////////////////////////////////////////////
+    /// NOTE: this function call is needed for environment-specific fixes //
+    init_threads();                                                       //
+    ////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///                Exemplu de utilizare cod generat                     ///
+    ///////////////////////////////////////////////////////////////////////////
+    Helper helper;
+    helper.help();
+    ///////////////////////////////////////////////////////////////////////////
+
+    SomeClass *c = getC();
+    std::cout << c << "\n";
+    delete c;
+
+    sf::RenderWindow window;
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
+    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
+    /// This is needed so we do not burn the GPU                            ///
+    // window.setVerticalSyncEnabled(true);                                 ///
+    window.setFramerateLimit(60);                                           ///
+    ///////////////////////////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////////
+    /// Pentru a putea folosi resurse locale în cadrul programului trebuie modificat `CMakeLists.txt`.
+    /// Deschide `CMakeLists.txt` și dă scroll la final, unde sunt apelurile la funcția `copy_files()`.
+    ///
+    /// După pasul anterior, putem folosi resursele locale folosind sintaxa `ResourceManager::Instance().get*`, unde
+    /// * poate fi Texture, Font, Sound sau orice alt tip de resursă de care avem nevoie.
+    ////////////////////////////////////////////////////////
+
+    // Decomentează liniile marcate cu `@` de mai jos pentru a vedea ce se întâmplă atunci când încerci să primești o textură inexistentă.
+
+    sf::Font font = ResourceManager::Instance().getFont("FiraSans-Regular.ttf");
+
+    sf::Texture avion_texture;
+    sf::Text tip_avion(font);
+    // sf::Texture dummyTexture; // @
+
+    try {
+        avion_texture = ResourceManager::Instance().getTexture("airplane.png");
+        // dummyTexture = ResourceManager::Instance().getTexture("fizzbuzz.png"); // @
+    } catch(std::exception& exp) {
+        std::cout << exp.what() << std::endl;
+        return 0;
+    }
+
+    sf::Sprite avion(avion_texture);
+
+    PersonalizeazaAvion(avion); // Este o funcție care modifică dimensiunea avionului și îi pune originea în centru.
+
+    tip_avion.setString("An airplane");
+
+    while(window.isOpen()) {
+        bool shouldExit = false;
+
+        while(const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+            else if (event->is<sf::Event::Resized>()) {
+                std::cout << "New width: " << window.getSize().x << '\n'
+                          << "New height: " << window.getSize().y << '\n';
+            }
+            else if (event->is<sf::Event::KeyPressed>()) {
+                const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
+                std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
+                if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    shouldExit = true;
+                }
+            }
+        }
+
+        if(shouldExit) {
+            window.close();
+            break;
+        }
+
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(150ms);
+
+
+        float new_angle = avion.getRotation().asDegrees() + 1;
+        sf::Angle angle = sf::degrees(new_angle);
+        avion.setRotation(angle);
+
+        window.clear();
+
+        window.draw(avion); // Afișăm avionul încărcat pe ecran
+        window.draw(tip_avion); // Afișăm tipul avionului
+
+        window.display();
+    }
+
+    return 0;
+}
